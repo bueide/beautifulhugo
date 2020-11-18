a90873183fbf9c59a8eac803146f63917a6ba731
Eric Bueide <eric@buei.de>
2020-11-17 19:48:16 -0500

    Dark mode JS

--- static/js/main.js
+++ static/js/main.js
@@ -175,43 +175,41 @@ var main = {
     $(".img-desc").hide();
   }
   }
-
-  const toggleColorMode = e => {
-    // Switch to Light Mode
-    if (e.currentTarget.classList.contains("light--hidden")) {
-      // Sets the custom html attribute
-      document.documentElement.setAttribute("color-mode", "light");
-      
-      // Sets the user's preferences in local storage
-      localStorage.setItem("color-mode", "light");
-      return;
-    }
-    
-    /* Switch to Dark Mode
-    Sets the custom html attribute */
-    document.documentElement.setAttribute("color-mode", "dark");
-    
-    // Sets the user's preference in local storge 
-    localStorage.setItem("color-mode", "dark");
-  };
-  
-  // Get the buttons in the DOM
-  const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
-  
-  // Set up event listeners
-  toggleColorButtons.forEach(btn => {
-    btn.addEventListener("click", toggleColorMode);
-  });
-  
-  // Respect user's color scheme preference 
-  if (
-    localStorage.getItem('color-mode') === 'dark' ||
-    (window.matchMedia( '(prefers-color-scheme: dark)').matches && !localStorage.getItem('color-mode'))
-  ) {
-    document.documentElement.setAttribute('color-mode', 'dark')
-  }
 };
 
+const toggleColorMode = e => {
+  // Switch to Light Mode
+  if (e.currentTarget.classList.contains("light--hidden")) {
+    // Sets the custom html attribute
+    document.documentElement.setAttribute("color-mode", "light");
+    
+    // Sets the user's preferences in local storage
+    localStorage.setItem("color-mode", "light");
+    return;
+  }
+  
+  /* Switch to Dark Mode
+  Sets the custom html attribute */
+  document.documentElement.setAttribute("color-mode", "dark");
+  
+  // Sets the user's preference in local storge 
+  localStorage.setItem("color-mode", "dark");
+};
 
+// Get the buttons in the DOM
+const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
+
+// Set up event listeners
+toggleColorButtons.forEach(btn => {
+  btn.addEventListener("click", toggleColorMode);
+});
+
+// Respect user's color scheme preference 
+if (
+  localStorage.getItem('color-mode') === 'dark' ||
+  (window.matchMedia( '(prefers-color-scheme: dark)').matches && !localStorage.getItem('color-mode'))
+) {
+  document.documentElement.setAttribute('color-mode', 'dark')
+}
 
 document.addEventListener('DOMContentLoaded', main.init);
